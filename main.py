492750125f051e67e1742a698ebc9d74b85fea1a
DymuX17 <patrykd1707@gmail.com>
2024-11-11 19:28:57 +0100

    Changing refresh process from external library to built in st.fragment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from visualise_data import Visualise
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     visualise = Visualise()
     visualise.plot_sin()
--- visualise_data.py
+++ visualise_data.py
@@ -1,26 +1,32 @@
 import influxdb_client, os, time
 import streamlit as st
+from streamlit import fragment
 import pandas as pd
 from init_db_conn import InitDB
-from streamlit_autorefresh import st_autorefresh
+import datetime
+
 
 
 class Visualise(InitDB):
     def __init__(self):
         super().__init__()
+        st.set_page_config("st.fragment_visualisation", page_icon="*")
 
     def plot_sin(self):
-        st_autorefresh(interval=2000, key="st.session_state.df")
+        st.header('Sinus visualisation')
+        st.caption("Every second chart is updated with latest data from `InfluxDB`")
 
         chart_placeholder = st.empty()
 
-        if "df" not in st.session_state:
+        if st.button("Reset streaming") or "df" not in st.session_state:
             st.session_state.df = pd.DataFrame([])
 
-        while True:
+        # while True:
+        @st.fragment(run_every=1)
+        def write_chart():
             query = f"""
             from(bucket: "{self.bucket}")
-            |> range (start: -5m)
+            |> range (start: -1m)
             """
             data = []
             tables = self.query_api.query(query)
@@ -31,14 +37,11 @@ class Visualise(InitDB):
                 st.session_state.df = pd.DataFrame(data)
 
                 st.session_state.df['time'] = pd.to_datetime(st.session_state.df['time'])
-                st.session_state.df['time_formatted'] = st.session_state.df['time'].dt.strftime('%H:%M:%S:%MS')
+                st.session_state.df['time_formatted'] = st.session_state.df['time'].dt.strftime('%H:%M:%S')
                 st.session_state.df = st.session_state.df.drop(columns=['time'])
+                st.scatter_chart(st.session_state.df, x='time_formatted', y='value', color='#add8e6',
+                                 use_container_width=True)
 
-                with chart_placeholder:
-                    st.header('Sinus visualisation')
-                    st.scatter_chart(st.session_state.df, x='time_formatted', y='value', color='#add8e6',
-                                     use_container_width=True)
+        with chart_placeholder:
+            write_chart()
 
-            print(st.session_state.df.dtypes)
-
-            time.sleep(2)
